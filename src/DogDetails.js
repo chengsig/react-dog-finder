f28213db957fb6569be711bcb5d7e440b15c917f
annika <annikaslund@gmail.com>
2019-03-26 16:28:28 -0700

    puppies

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import uuid from "uuid/v4";
 import { Link } from "react-router-dom";
 
 class DogDetails extends Component {
   render() {
+    let oneDog = this.props.dogData.find(dog => dog.name.toLowerCase() === this.props.match.params.name)
     return (
       <div className="DogDetails">
-        
+          <img src={oneDog.src} alt={oneDog.name}></img>
+          <h1>{oneDog.name}</h1>
+          <h2>{oneDog.age} years old</h2>
+          <ul>
+              {oneDog.facts.map(fact => (
+                  <li key={ uuid() }>{ fact }</li>
+              ))}
+          </ul>
+          <Link to="/dogs">Go Back</Link>
       </div>
     );
   }
--- src/DogList.js
+++ src/DogList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import DogDetails from './DogDetails';
 
 class DogList extends Component {
   render() {
@@ -10,7 +9,7 @@ class DogList extends Component {
         {this.props.dogData.map(dog => (
             <div className="DogList-details">
                 <img src={dog.src} alt={dog.name} />
-                <Link to={`/dogs/${dog.name}`}>{dog.name}</Link>
+                <Link to={`/dogs/${dog.name.toLowerCase()}`}>{dog.name}</Link>
             </div>
         ))}
       </div>
