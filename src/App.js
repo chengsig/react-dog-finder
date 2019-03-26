9c317266e99cb111a667aa4df0f79e165768b243
annika <annikaslund@gmail.com>
2019-03-26 16:42:47 -0700

    Add redirect for dog that doesn't exist

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
             <Route exact path="/dogs"
               render={() => <DogList dogData={this.props.dogs} />}
             />
-            <Route path="/dogs/:name"
+            <Route exact path="/dogs/:name"
               render={routeProps => <DogDetails {...routeProps} 
                                             dogData={this.props.dogs} />}
             />
